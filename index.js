c7bbaf73b9c0b1c290f9cdfba2b073da0f2a6f39
MrCreating <44828493+MrCreating@users.noreply.github.com>
2021-09-28 16:54:00 +0300

    Created the config file

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
+console.log('[OK] Application is loaded, now reading the config');
+
+const API = require('./link-shorter/api.js');
 const LinkShorter = require('./link-shorter/linkShorter.js');
+
+let config = null;
+try {
+	config = require('./config.json');
+	console.log('[OK] config file found and parsed successfully.');
+} catch (e) {
+	console.log('[!] Config file loading failed.');
+	process.exit(1);
+}
+
+if (!config.server) {
+	console.log('[!] Server block in config is required.');
+	process.exit(1);
+}
+if (!config.database) {
+	console.log('[!] Database block in config is required.');
+	process.exit(1);
+}
+
+if (!config.server.domain || config.server.port) {
+	console.log('[!] Specify the domain and port in config.');
+	process.exit(1);
+}
+
+if (!config.database.host || !config.database.user || !config.database.password) {
+	console.log('[!] Specify database host, username and password in config.');
+	process.exit(1);
+}

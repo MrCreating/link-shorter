3940145367decc927775e4ae79ea1fe0eab164dd
MrCreating <44828493+MrCreating@users.noreply.github.com>
2021-09-28 15:17:05 +0300

    Updated class

---
+++ linkShorter.js
@@ -0,0 +1,33 @@
+/**
+ * Main source code file.
+ *
+ * Class LinkShorter
+ * method: createLink (readableUrl = null)
+ * Creates a shorted link fron url 
+ * return: string query or user-given url
+ *
+ * method: getLink (query - required string)
+ * Get a URL by query from DB or cache.
+ * return: url string, it can be used to redirect.
+*/
+
+/**
+ * Used MySQL table:
+ * links.data (id BIGINT PRIMARY KEY AUTOINCREMENT, query TEXT, url TEXT);
+*/
+
+module.exports = class LinkShorter {
+	#currentDataBAseConnection;
+	#currentCacheConnection;
+
+	constructor (databaseConnection = null, cacheConnection = null) {
+		const ConnectionManager = require('./connectionManager.js');
+
+		if (!databaseConnection) {
+			this.#currentDataBAseConnection = ConnectionManager.getDBConnection('', '', '', '');
+		}
+		if (!cacheConnection) {
+			this.#currentCacheConnection = ConnectionManager.getCacheConnection();
+		}
+	}
+}
